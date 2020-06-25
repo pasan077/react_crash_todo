32348bb184426d31708e8052c5c2101bf34c6bad
Pasan Jayathilaka <opasan4all@gmail.com>
2020-06-25 16:30:17 +0530

    “25-06-2020-1630hrs”

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Todos from './components/Todos';
 
+
 function App() {
-  const [todos, setTodos] = useState([{ text: 'Learn Hooks' }])
+  const [todos, setTodos] = useState([{ id:1, title: 'take out the trash'},{id:1, title: 'take out the trash'}]);
   return (
     <div className="App">
      <Todos />
       </div>
-  );
+  );  
 }
 
 export default App;
