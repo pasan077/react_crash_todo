cff39fbe8b19ec38632ba7ec3832d888ee850aea
Pramuka Jayathilaka <opasan4all@gmail.com>
2020-07-29 14:42:11 +0530

    29-07-2020 1442hrs
    29-07-2020 1442hrs

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import Todos from './components/Todos';
 
 
 function App() {
-  const [todos, setTodos] = useState([
-    {id:1, 
-    title: 'take out the trash'},
-    {id:2, 
-    title: 'take out the trash'},
-    {id:3, 
-    title: 'pay the bills'},
-    
+  let [todos, setTodos] = useState([
+    {
+      id:1, 
+    title: 'take out the trash',
+     completed: false
+    },
+    {
+      id:2, 
+    title: 'Dinner with wife',
+    completed: false
+  },
+    {
+      id:3, 
+    title: 'Meeting with boss',
+    completed: false
+  },
   ]);
+
   return (
     <div className="App">
      <Todos />
